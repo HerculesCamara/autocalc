8505a6ccc87471f966f2211bb3c15b869b557ed8
Guilherme F. Trindade <38335563+GuilhermeFT@users.noreply.github.com>
2021-08-26 00:34:37 -0300

    install favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,6 +37,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" href="/favicon.png" type="image/png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
