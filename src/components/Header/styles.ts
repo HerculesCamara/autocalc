bacc363d47c9fde2789e38a0fb92c6f3484ef77c
Hércules Câmara <39931772+HerculesCamara@users.noreply.github.com>
2021-08-27 01:17:32 -0300

    Final version header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,6 +1,13 @@
 import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  padding: 36px;
+
   .img {
     margin-top: auto;
     max-width: 320px;
---
+++ src/pages/materias2.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from 'next'
+
+import { Header } from '@components/Header'
+import { MainCard } from '@components/MainCard'
+
+const Materias: NextPage = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <MainCard> Salve</MainCard>
+      </main>
+    </>
+  )
+}
+
+export default Materias
