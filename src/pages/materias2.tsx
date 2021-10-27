542ecd10c7c04f6ff4675f87bb3997ac18c9a5cb
Hércules Câmara <39931772+HerculesCamara@users.noreply.github.com>
2021-10-27 18:39:04 -0300

    fix: materias2

--- src/pages/materias2.tsx
+++ src/pages/materias2.tsx
@@ -2,7 +2,6 @@ import { NextPage } from 'next'
 
 import { Header } from '@components/Header'
 import { MainCard } from '@components/MainCard'
-import { SearchBar } from '@components/SearchBar'
 
 const Materias: NextPage = () => {
   return (
