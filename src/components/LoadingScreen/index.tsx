d5b20d934fe6c7c2e8f1b8449b5c2b1d4f362b9a
guilhermeft <guilhermetrind12@gmail.com>
2022-01-08 23:29:35 -0300

    feat: create LoadingScreen component

---
+++ src/components/LoadingScreen/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles'
+
+const LoadingScreen = () => (
+  <S.Wrapper>
+    <S.Container>
+      <S.Logo />
+      <S.Loadbar />
+    </S.Container>
+  </S.Wrapper>
+)
+
+export default LoadingScreen
